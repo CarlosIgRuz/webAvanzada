d1c902afbc8ccc79a450ceacabc9c469d42637aa
Carlos Ruz Aguilera <charliiruz@gmail.com>
2026-09-07 14:31:30 +0000

    fix: corrige prueba del frontend

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Texto incorrecto de prueba');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Catálogo de Recursos');
   });
 });
